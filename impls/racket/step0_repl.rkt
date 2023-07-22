089ed385855898c09ea845360b81a07ff4494f4b
Reza <rezahandzalah@gmail.com>
2023-07-22 17:09:59 +0700

    step0 manual test ok

---
+++ impls/racket/step0_repl.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(define (READ str) str)
+(define (EVAL str) str)
+(define (PRINT str) str)
+(define (rep input) (PRINT (EVAL (READ input))))
+
+(define (loop)
+	(display "user>")
+	(define input (read-line))
+	(unless (eof-object? input)
+		(displayln (rep input))
+		(loop)))
+
+(loop)
