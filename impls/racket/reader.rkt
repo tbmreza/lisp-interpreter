0d5ff907df27b3081f896c7ebb3ab352f3a367b8
Reza <rezahandzalah@gmail.com>
2023-08-07 20:17:23 +0700

    read-form read-list

--- impls/racket/reader.rkt
+++ impls/racket/reader.rkt
@@ -1,6 +1,8 @@
 #lang racket
 
+(require rackunit)
 (require racket/generic)
+
 (require "types.rkt")
 
 (define pat
@@ -20,14 +22,12 @@
 	(define tokens (regexp-match* pat str #:match-select cadr))
 	(filter (lambda (x) (not (equal? x ""))) tokens))
 
-(tokenize "~@  bool")
-(tokenize "~@  ]")
-(tokenize ",,(  + 2   (*  3  4)  )")
+(check-equal? 9 (length (tokenize ",,(  + 2   (*  3  4)  )")))
 
 (define-generics self
-	[read-str self input]
-
+	; [at-end self]
 	[read-form self]
+	; [read-form self acc]
 	[peek self]
 	[next self])
 
@@ -40,36 +40,57 @@
 			  'comment
 			  'word) v))
 
-(define/contract (make-token token-variant data)
-  (-> token-variant? string? token?)
-  (token token-variant data))
+(define/contract (read-atom v)
+	(-> string? scalar?)
+	(match (string->number v)
+		[#f	(string->symbol v)]
+		[parsed	parsed]))
+
+(define/contract (read-str input)
+	(-> string? ast?)
+	(define rs (reader-state (tokenize input) 0))
+	(read-form rs))
+
+; "repeatedly call `read_form` with the Reader object until it
+; encounters a ')' token"
+; contract: pos is open paren
+; -> list of tokens between parens
+(define (read-list rs)
+	(define (h rs acc)
+		(next rs)
+		(define (f v)
+			(append (list (read-atom v))
+				(h rs acc)))
+		(match (peek rs)
+			[")"	acc]
+			[v	(f v)]))
+	(h rs '()))
 
 (struct reader-state (tokens [pos #:mutable])
 	#:transparent
 	#:methods gen:self [
-		(define/contract (read-str self input)
-			(-> struct? string? ast?)
-			; (list 12 22))
-			(hash-set (hash) "hh" "12"))
-			; (hash))
-		; (define (read-list)
-		;   (read-form)
-		; read anything but parens?
-		(define (read-form self)
-			(define first-char (string-ref (peek self) 0))
-			(match first-char
-			  ["(" #t]
-			  ; ["(" (read-list self)]
-			  [_ #f]
-			  )
-			"hah"
-		)
+		(define/contract (read-form self)
+			(-> struct? ast?)
+			(define current-token (peek self))
+			(match (string-ref current-token 0)
+				[#\( (read-list self)]
+				[_   (read-atom (peek self))])
+			)
+
 		(define (peek self)
 			(list-ref	(reader-state-tokens self)
 					(reader-state-pos self)))
+
 		(define (next self)
 			(set-reader-state-pos!	self
 						(add1 (reader-state-pos self)))
 			(peek self))])
 
-(provide reader-state read-str read-form make-token)
+(check-equal? (read-str ",,(+ 15 16)") '(+ 15 16))
+; (map (lambda (x) (displayln (symbol? x))) (read-str ",,(+ 15 ,16 isOk)"))
+
+(provide reader-state read-str tokenize)
+
+(check-equal?
+	(read-list (reader-state '("(" "+" "16" "12" ")") 0))
+	'(+ 16 12))
